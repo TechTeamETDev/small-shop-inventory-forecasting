43b2ad1e1a52a952ac64e3c6678467618d63b5ec
sebrinamusbah <sebrinm9@gmail.com>
2026-02-18 22:56:16 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,11 +10,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
- ->withMiddleware(function (Middleware $middleware) {
-    $middleware->alias([
-        'role' => \App\Http\Middleware\CheckRole::class,
-    ]);
+ ->withMiddleware(function ($middleware) {
+    $middleware->alias('role', \App\Http\Middleware\CheckRole::class);
 })
+
 
 
     ->withExceptions(function (Exceptions $exceptions): void {
